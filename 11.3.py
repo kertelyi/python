70db77db3c0a2a3b08eee1ce6cf5c8c03dc8febc
kertelyi <a.alyoshin@list.ru>
2019-11-23 16:22:36 +0300

    11.3 has been added

---
+++ 11.3.py
@@ -0,0 +1,9 @@
+word = input()
+i = int(input())
+letter = input()
+if i > len(word) or len(letter) > 1 or i < 0:
+    print('Ошибка')
+elif word.lower()[i - 1] == letter.lower():
+    print('да')
+else:
+    print('нет')
